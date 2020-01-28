baabe45f5580557b18d458a405210cb72bfb5e87
ossoso <lossososoi@gmail.com>
2020-01-28 14:50:14 +0200

    cypress wait commands

--- dash-renderer/cypress/support/commands.js
+++ dash-renderer/cypress/support/commands.js
@@ -1,3 +1,24 @@
+//     def __init__(self, selector, text):
+//         self.selector = selector
+//         self.text = text
+
+//     def __call__(self, driver):
+//         try:
+//             elem = driver.find_element_by_css_selector(self.selector)
+//             logger.debug(
+//                 "text to equal {%s} => expected %s", elem.text, self.text
+//             )
+//             return (
+//                 str(elem.text) == self.text
+//                 or str(elem.get_attribute("value")) == self.text
+//             )
+//         except WebDriverException:
+//             logger.exception("text_to_equal encountered an exception")
+//             return False
+
+
+
+
     // def _wait_for(self, method, args, timeout, msg):
     //     """Abstract generic pattern for explicit WebDriverWait."""
     //     _wait = (
@@ -13,8 +34,19 @@
     //     )
 
 
-const wait_for_text_to_equal = () => {
-    
+
+const wait_for_text_to_equal = (selector, text, timeout=null) => {
+    try {
+        cy.get(selector).wait(timeout=timeout).as(textEl)
+        cy.get('@textEl').
+        its('text')
+        // logger.debug(
+        //     "text to equal {%s} => expected %s", elem.text, self.text
+        // )
+
+    } catch (responseTimout) {
+        
+    }
 }
 // ***********************************************
 // This example commands.js shows you how to
